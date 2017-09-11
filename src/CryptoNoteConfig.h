ed32c36aac114887910c91faf7d1b8a1f776ce7f
pktcoin <31813170+pktcoin@users.noreply.github.com>
2017-09-11 12:07:33 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "172.104.188.201:17635",
+  "172.104.183.96:17635",
   //"your_seed_ip2.com:8080",
 };
 
